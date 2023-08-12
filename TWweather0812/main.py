b42cb8ea0b0f26ba2e4bfe6648f29cb5a2389c11
ahaing <ahahaing@gmail.com>
2023-08-12 06:57:38 +0000

    pandas & streamlit

--- TWweather0812/main.py
+++ TWweather0812/main.py
@@ -3,6 +3,7 @@ import csv
 from datetime import datetime
 import pytz
 import os
+import pandas as pd
 
 def download_data()->dict:
     url = 'https://opendata.cwb.gov.tw/fileapi/v1/opendataapi/F-C0032-001?Authorization=rdec-key-123-45678-011121314&format=JSON'
